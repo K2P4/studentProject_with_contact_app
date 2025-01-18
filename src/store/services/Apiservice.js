498bc6dd208aea9265662b10aa4fc0ea900a2033
Phyo Thura <155300367+K2P4@users.noreply.github.com>
2025-01-18 15:25:40 +0630

    Update Apiservice.js

--- src/store/services/Apiservice.js
+++ src/store/services/Apiservice.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Apiservice = createApi({
 	reducerPath: "api",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "https://contact.sankyitar.store/api/v1/",
+		baseUrl: "https://contact-app-eight-pi.vercel.app/api/api",
 
 		prepareHeaders: (headers, { getState }) => {
 			const token = localStorage.getItem("token");
