097a7d011f0d145d1f1ca64d9bd381b25c4bcb26
Phyo Thura <155300367+K2P4@users.noreply.github.com>
2025-03-08 22:05:55 +0630

    api route update

--- src/store/services/Apiservice.js
+++ src/store/services/Apiservice.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Apiservice = createApi({
 	reducerPath: "api",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "https://contact-app-eight-pi.vercel.app/api/api",
+		baseUrl: "http://127.0.0.1:8000/api",
 
 		prepareHeaders: (headers, { getState }) => {
 			const token = localStorage.getItem("token");
